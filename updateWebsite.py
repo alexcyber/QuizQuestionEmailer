2922b6c766927bb6996d992fb39fda31389cc98f
Alpha <32499495+alphaQ101@users.noreply.github.com>
2019-07-23 01:18:10 -0400

    Compatability update
    + Moved all customizing to the config.txt file
    - Removed traces hard coded values
    + Added variables websiteRootDirectory, motd, footnote to config.txt
    + Added MOTD and Footnote capability to websiteTemplate
    + Fixed notes (ish)
    + Fixed questionpool.csv hard coding issue

--- updateWebsite.py
+++ updateWebsite.py
@@ -1,5 +1,3 @@
-
-
 '''
 This class will update nginx files on a remote linux server to create weburls for answers.
 init(self, url, username, publickey, rootWebDirectory, question)
@@ -19,25 +17,24 @@ First run through will create webpages at a local level to provide faster turnar
 init(self, url
 '''
 
-import subprocess
-
 class updateWebsite:
 
-    #def __init__(self, url, username, publickey, rootWebDirectory, question):
     def __init__(self, folder, url, problem):
         self.url = url
         self.problem = problem
         self.folder = folder
 
     #creates html file for webpages/{url} using template at webpageTemplate.html
-    def createHTMLPage(self, template):
+    def createHTMLPage(self, template, motd, footnote):
 
         #check to see if HTML page is already created
         '''
         create html file at location webpages/*unique url*.  Overwrites if needed
-            Uses template located at webpageTemplate.html and replaces:
-                text "Question Here" with question and choices
-                text "Answer Here" with answer and reason (if one)
+            Uses template specified in config.txt and replaces:
+                text {MOTD Here} with motd from config (html encoded)
+                text {Question Here} with question and choices from config.txt[questionPool]
+                text {Answer Here} with answer and reason (if one) from config.txt[questionPool]
+                text {Footnote Here} with footnote from config (html encoded)
         '''
 
         htmlAnswer = open(f"{self.folder}/{self.url}.html", 'w')
@@ -48,7 +45,7 @@ class updateWebsite:
             try:
                 if "Answer:" in problemSplit[i-1]:
                     boldAnswer = problemSplit[i].split()
-                    boldAnswer.pop(0)
+                    #boldAnswer.pop(0)  Unknown reason why I put this here.  Leaving for now
                     boldAnswer = ' '.join(boldAnswer)
                     boldAnswer = f"<b>{boldAnswer}</b>\n"
                     problemSplit[i] = boldAnswer
@@ -60,23 +57,19 @@ class updateWebsite:
                 print("Something went wrong splitting adding html tags to text")
         self.problem = ''.join(problemSplit)
         problemSplit = self.problem.split("Answer:")
+
+        #using the template, creates html page filling in the MOTD, question, answer, footnote
         for line in htmlTemplate:
-                if "{Question Here}" in line:
+                if "{MOTD Here}" in line:
+                    htmlAnswer.write(motd)
+                elif "{Question Here}" in line:
                     htmlAnswer.write(problemSplit[0])
                 elif "{Answer Here}" in line:
                     try:
                         htmlAnswer.write(problemSplit[1])
                     except:
                         print("WARNING: No answer to this question")
+                elif "{Footnote Here}" in line:
+                    htmlAnswer.write(footnote)
                 else:
                     htmlAnswer.write(line)
-
-#    def upload(self, remoteHost, user, lFileLocation, rFileLocation, keyFileLocation):
-#        keyFile = open(keyFileLocation, "r")
-#        p = subprocess.Popen(["pscp", "-i",
-#                              r"C:\Users\mmoli\.ssh\mailbotAmazon.ppk",
-#                              "C:\\Users\\mmoli\\OneDrive\\Documents\\Programming Projects\\Python\\Projects\\Daily CCNA Question\webpages\\test.html",
-#                              "ec2-user@ec2-18-219-217-61.us-east-2.compute.amazonaws.com:/home/ec2-user/mailbotAnswers/webpages/test.html"],
-#                             shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-#        print(p.communicate())
